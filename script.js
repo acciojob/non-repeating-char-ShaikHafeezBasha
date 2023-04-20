87e27aaa52c9e7ab8586e1222fb10f87580220fd
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-04-20 07:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let str=prompt("Enter a string")
+for(let i=0;i<str.length;i++){
+	let char=str[i];
+	if(indexOf(char)==i && str.indexOf(char,i+1)==-1){
+		console.log(alert(char))
+		return;
+	}
+}
+
